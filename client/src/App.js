ce2b4c15a8512765c105b408c2136fa256b43297
ilover311 <ilover311@gmail.com>
2017-10-02 18:14:29 +0900

    Edited README.md and added Router

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,18 @@ import AppBar from 'material-ui/AppBar'
 import Drawer from 'material-ui/Drawer';
 import FlatButton from 'material-ui/FlatButton';
 import MenuItem from 'material-ui/MenuItem';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+  matchPath
+} from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React, { Component } from 'react';
 import './App.css';
+
+import SearchRoom from './SearchRoom';
 
 class App extends Component {
   constructor(props) {
@@ -21,24 +30,29 @@ class App extends Component {
     })
   }
 
-
-
   render() {
     return (
-      <MuiThemeProvider>
-        <AppBar
-          title="회의실 예약 SAP"
-          onLeftIconButtonTouchTap={this.leftTapButton.bind(this)}
-        />
-        <Drawer
-          docked={false}
-          width={200}
-          open={this.state.drawerOpen}
-          onRequestChange={(open) => this.setState({drawerOpen: open})}
-        >
-          <MenuItem onClick={() => this.setState({drawerOpen: false})}>Menu Item</MenuItem>
-        </Drawer>
-      </MuiThemeProvider>
+      <BrowserRouter>
+        <MuiThemeProvider>
+          <div>
+            <AppBar
+              title="회의실 예약 SAP"
+              onLeftIconButtonTouchTap={this.leftTapButton.bind(this)}
+            />
+            <Drawer
+              docked={false}
+              width={200}
+              open={this.state.drawerOpen}
+              onRequestChange={(open) => this.setState({drawerOpen: open})}
+            >
+              <MenuItem onClick={() => this.setState({drawerOpen: false})}>Menu Item</MenuItem>
+            </Drawer>
+            <Switch>
+              <Route path="/" component={SearchRoom}/>
+            </Switch>
+          </div>
+        </MuiThemeProvider>
+      </BrowserRouter>
     );
   }
 }
---
+++ client/src/SearchRoom.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SearchRoom extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        tmp
+      </div>
+    );
+  }
+};
+
+export default SearchRoom;
